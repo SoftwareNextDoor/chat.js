241be26d9d214a5085066a5d0b5d93b8f5214e8c
Alan Andrade <alan.andradec@gmail.com>
2013-01-09 15:52:53 -0600

    super simple version with messages list

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,39 @@
-window.App = Ember.Application.create()
+var App = Ember.Application.create()
+
+
+App.Router.map(function( match ){
+  match('/').to('index')
+});
+
+App.IndexRoute = Em.Route.extend({
+  setupController: function( controller ){
+  },
+
+  renderTemplate: function(){
+    var boardController = this.controllerFor('board');
+
+    this.render('board', {
+      controller: boardController,
+      outlet: 'board'
+    });
+
+  }
+
+});
+
+App.MessageInputView = Em.TextField.extend({
+  clean: function(){ this.set('value', ''); },
+  keyUp: function(e){
+    if ( this.get('value') !== '' && e.keyCode === 13 ){
+      this.get('controller').addMessage( this.get('value') );
+      this.clean();
+    }
+  }
+}),
+
+App.BoardController = Ember.ArrayController.extend({
+  content: [],
+  addMessage: function( msg ){
+    this.content.pushObject( msg );
+  }
+});
