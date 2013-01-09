27d26e2fcb9bf488b385c42095479b36608be1c4
Alan Andrade <alan.andradec@gmail.com>
2013-01-09 15:57:02 -0600

    cleanup

--- js/chat.js
+++ js/chat.js
@@ -6,33 +6,36 @@ App.Router.map(function( match ){
 });
 
 App.IndexRoute = Em.Route.extend({
-  setupController: function( controller ){
-  },
-
   renderTemplate: function(){
     var boardController = this.controllerFor('board');
-
     this.render('board', {
       controller: boardController,
       outlet: 'board'
     });
-
   }
-
 });
 
+
 App.MessageInputView = Em.TextField.extend({
-  clean: function(){ this.set('value', ''); },
+  clean: function(){
+    this.set('value', '');
+  },
+
+  sendMessage: function( msg ){
+    this.get('controller').addMessage( msg );
+  },
+
   keyUp: function(e){
     if ( this.get('value') !== '' && e.keyCode === 13 ){
-      this.get('controller').addMessage( this.get('value') );
+      this.sendMessage( this.get('value' ) );
       this.clean();
     }
   }
-}),
+});
 
 App.BoardController = Ember.ArrayController.extend({
   content: [],
+
   addMessage: function( msg ){
     this.content.pushObject( msg );
   }
